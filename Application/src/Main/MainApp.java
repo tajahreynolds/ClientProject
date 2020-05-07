c6e4752a80b4d5e51ebd0a1a64f4c23acad3d1bf
TaJah Reynolds <reynolt4@miamioh.edu>
2020-05-07 01:42:57 -0400

    fix error

--- Application/src/Main/MainApp.java
+++ Application/src/Main/MainApp.java
@@ -427,7 +427,7 @@ public class MainApp {
 		tf.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				for (int i = 0; i < originList.size(); i++) {
-					if (originList.get(i).getTitle() != tf.getText();
+					if (originList.get(i).getTitle() != tf.getText())
 					    originList.remove(i);
 				}
 				loadMainApp();
